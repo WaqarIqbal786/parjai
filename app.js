760e6457ecfe1e2c1a6bc68d9b6fdb41d7543300
WaqarIqbal786 <waqariiqbal786@gmail.com>
2023-03-31 02:51:25 -0700

    first

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn');
+const btn2 = document.querySelector('#btn2');
+
+btn.addEventListener("click",move);
+function move() {
+    btn2.innerHTML = `Love You Shahzadi Esho ❤️ <br> Ummmmmah <br> Bs bs roza ay!	`;
+}
